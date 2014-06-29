1987ba194025bc120d6fb68bbc767633961cc9fa
CodingAnarchy <mtanous22@gmail.com>
2014-06-28 19:46:54 -0600

    Update to fix which pins were assigned to the EQ chip.

--- equalizer/equalizer.ino
+++ equalizer/equalizer.ino
@@ -14,10 +14,10 @@ const byte pins[] = {
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(60, pins[0], NEO_GRB + NEO_KHZ800);
 
 // eq stuff
-const int strobe = A0; // strobe pins on digital 4
-const int res = A1; // reset pins on digital 5
+const int strobe = A1; // strobe pins on digital 4
+const int res = A2; // reset pins on digital 5
 int band;
-int EQ_OUT1 = A2;
+int EQ_OUT1 = A0;
 
 int eqLevels[7]; // store band values in these arrays
 
@@ -29,8 +29,8 @@ void led_setup(){
 
 void initMSGEQ7()
 {
-  digitalWrite(res,LOW); // reset low
-  digitalWrite(strobe,HIGH); //pin 5 is RESET on the shield
+  digitalWrite(res, LOW); // reset low
+  digitalWrite(strobe, HIGH); //pin 5 is RESET on the shield
 }
 
 void readMSGEQ7()
@@ -40,10 +40,10 @@ void readMSGEQ7()
   digitalWrite(res, LOW);
   for(band=0; band <7; band++)
   {
-    digitalWrite(strobe,LOW); // strobe pin on the shield - kicks the IC up to the next band 
+    digitalWrite(strobe, LOW); // strobe pin on the shield - kicks the IC up to the next band 
     delayMicroseconds(30); // 
-    eqLevels[band] = analogRead(EQ_OUT1); // store left band reading
-    digitalWrite(strobe,HIGH); 
+    eqLevels[band] = analogRead(EQ_OUT1); // store band reading
+    digitalWrite(strobe, HIGH); 
   }
 #ifdef DEBUG
   dumpEQ();
@@ -107,26 +107,28 @@ void loop(){
 
       initMSGEQ7();
       readMSGEQ7();
-      for (int i = 0; i < 7; i = i + 2){
-        int level = map(eqLevels[i], 0, 600, 0, strip1.numPixels());
+      // for (int i = 0; i < 7; i = i + 2){
+      int level = map(eqLevels[2], 0, 1024, 0, strip1.numPixels());
 #ifdef DEBUG
-        for(int i = 0; i < level; i++){
-          strip1.setPixelColor(i, 0, 255, 255); // set to cyan
-        }
-        for(int i = level; i < strip1.numPixels(); i++){
-          strip1.setPixelColor(i, 0, 0, 0);
-        }
-
-        // Delay function to prevent flickering the LEDs too quickly
-        time = millis();
-        if(time - prev_time < 50){
-          delay(time - prev_time + 1);
-        }
-        prev_time = millis();
-        strip1.show();
-#endif
+      Serial.print("Level = ");
+      Serial.println(level);
+      for(int i = 0; i < level; i++){
+        strip1.setPixelColor(i, 0, 255, 255); // set to cyan
+      }
+      for(int i = level; i < strip1.numPixels(); i++){
+        strip1.setPixelColor(i, 0, 0, 0);
       }
 
+      // Delay function to prevent flickering the LEDs too quickly
+      /*time = millis();
+      if(time - prev_time < 50){
+        delay(time - prev_time + 1);
+      }
+      prev_time = millis();*/
+      delay(50);
+      strip1.show();
+#endif
+      //}
     }
     else {
       // Read in volume output and key off of that
@@ -137,6 +139,7 @@ void loop(){
 #endif
   }
 }
+
 
 
 
